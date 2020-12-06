ae5358663f52871ed6449b7286ea39719f8dd659
KevinAlanis <36685792+KevinAlanis@users.noreply.github.com>
2020-12-06 17:13:29 -0600

    Gestion cursos lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { GestionCursosComponent } from './gestion-cursos/gestion-cursos.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'VistasXtec';
+  title : string 
+ 
+
+  constructor() {}
+
 }
